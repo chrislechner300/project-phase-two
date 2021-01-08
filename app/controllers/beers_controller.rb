878775242906df3e6b5386644c6798b07656b7a1
chrislechner300 <denver929@gmail.com>
2021-01-08 16:45:37 -0700

    added to ber controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,12 +1,27 @@
 class BeersController < ApplicationController
 
+  get '/beers' do
+    @beers = Beer.all
+    erb :'beers/index'
+  end
 
-
+  
   get '/beers/new' do
     erb :'beers/new'
   end
-
+  
+  get '/beers/:id' do
+    @beer = Beer.find_by_id(params[:id])
+    erb :'beers/show'
+  end
+  
   post '/beers' do
+    beer = Beer.new(params[:beer])
     
+    if beer.save
+      redirect '/beers'
+    else
+      redirect '/beers/new'
+    end
   end  
 end
