4104506dd1d10173b5d15bc4d832091ffd5a465e
chrislechner300 <denver929@gmail.com>
2021-01-09 15:50:48 -0700

    added logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,17 @@ class SessionsController < ApplicationController
   end
 
   post '/signup' do
+    user = User.new(params[:user])
+      if user.save
+        session[:user_id] = user.id
+        redirect "/beers"
+      else
+        redirect "/signup"
+    end
+  end
 
+  get '/logout' do
+    session.clear
+    redirect '/'
   end
 end
