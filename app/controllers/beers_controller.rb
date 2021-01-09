173cb91ded7cc4c7c049e8d424ca9c699c22665c
chrislechner300 <denver929@gmail.com>
2021-01-09 12:57:36 -0700

    added delete

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -12,14 +12,16 @@ class BeersController < ApplicationController
 
   get '/beers/:id' do
     find_beer
+    redirect_if_beer_not_found
     erb :'beers/show'
   end
   
   get '/beers/:id/edit' do
     find_beer
+    redirect_if_beer_not_found
     erb :'beers/edit'
   end
-
+  
   post '/beers' do
     beer = Beer.new(params[:beer])
     
@@ -29,19 +31,30 @@ class BeersController < ApplicationController
       redirect '/beers/new'
     end
   end 
-
+  
   patch "/beers/:id" do
     find_beer
+    redirect_if_beer_not_found
     if @beer.update(params[:beer])
       redirect "/beers/#{@beer.id}"
     else
       redirect "/beers/#{@beer.id}/edit"
     end
   end
+
+  delete '/beers/:id' do
+    find_beer
+    @beer.destroy if @beer
+    redirect "/beers"
+  end
   
   private
-    def find_beer
-      @beer = Beer.find_by_id(params[:id])
-    end
-
+  def find_beer
+    @beer = Beer.find_by_id(params[:id])
+  end
+  
+  def redirect_if_beer_not_found
+    redirect "/beers" unless @beer
+  end
+  
 end
