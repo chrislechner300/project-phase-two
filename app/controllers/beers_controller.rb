3e609729b983694029d411e9987cdb10fcb882b0
chrislechner300 <denver929@gmail.com>
2021-01-09 12:14:00 -0700

    added edit

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -9,12 +9,17 @@ class BeersController < ApplicationController
   get '/beers/new' do
     erb :'beers/new'
   end
-  
+
   get '/beers/:id' do
-    @beer = Beer.find_by_id(params[:id])
+    find_beer
     erb :'beers/show'
   end
   
+  get '/beers/:id/edit' do
+    find_beer
+    erb :'beers/edit'
+  end
+
   post '/beers' do
     beer = Beer.new(params[:beer])
     
@@ -23,5 +28,20 @@ class BeersController < ApplicationController
     else
       redirect '/beers/new'
     end
-  end  
+  end 
+
+  patch "/beers/:id" do
+    find_beer
+    if @beer.update(params[:beer])
+      redirect "/beers/#{@beer.id}"
+    else
+      redirect "/beers/#{@beer.id}/edit"
+    end
+  end
+  
+  private
+    def find_beer
+      @beer = Beer.find_by_id(params[:id])
+    end
+
 end
